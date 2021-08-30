c21a080c52f3e1edd1e2384820c02a8950b22b99
sammieghabra <sammieg@umich.edu>
2021-08-29 22:17:52 -0700

    read from storage and use the content as the text for speech

--- src/main/java/PDYPMain.java
+++ src/main/java/PDYPMain.java
@@ -1,24 +1,36 @@
 import com.google.api.gax.core.CredentialsProvider;
 import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import com.google.cloud.texttospeech.v1.*;
+import com.google.common.collect.Lists;
 import com.google.protobuf.ByteString;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 
 public class PDYPMain {
 
     public static void main(String... args) throws Exception {
+        final String credentialPath = args[0];
+        final String textFile = args[1];
+
+        String content = getContent(credentialPath, textFile);
+
         // Instantiates a client
         CredentialsProvider credentialsProvider = FixedCredentialsProvider.create(ServiceAccountCredentials.fromStream(
-                new FileInputStream(args[0])));
+                new FileInputStream(credentialPath)));
         TextToSpeechSettings settings = TextToSpeechSettings.newBuilder().setCredentialsProvider(credentialsProvider).build();
 
         try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create(settings)) {
             // Set the text input to be synthesized
-            SynthesisInput input = SynthesisInput.newBuilder().setText("Hey you!").build();
+            SynthesisInput input = SynthesisInput.newBuilder().setText(content).build();
 
             // Build the voice request, select the language code ("en-US") and the ssml voice gender
             // ("neutral")
@@ -48,4 +60,16 @@ public class PDYPMain {
         }
     }
 
+    static String getContent(String jsonPath, String textFile) throws IOException {
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(jsonPath))
+                .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+
+        String bucketName = "pdyp-scripts";
+        Bucket bucket = storage.get(bucketName);
+
+        Blob blob = bucket.get(textFile);
+        final String content = new String(blob.getContent());
+        return content;
+    }
 }
